b406a6eafe71fb616ae8dfc3e505242b0ebb4d4c
weilingshine <weiling.go@gmail.com>
2022-09-30 21:27:56 +0800

    20220930_測試機

---
+++ js/header.js
@@ -0,0 +1,34 @@
+header = '<header>\
+    <div class="container">\
+        <div class="mob-header">\
+            <a href="javascript:;" class="slidebar-btn" data-bs-toggle="collapse" data-bs-target="#collapseWidthExample" aria-expanded="false" aria-controls="collapseWidthExample"><span></span></a>\
+            <a class="brand-logo" href="https://www.1111.com.tw/" title="1111 科技專區" target="_blank"><img src="images/logo01.png"></a>\
+        </div>\
+        <div class="sidebar-slide collapse collapse-horizontal" id="collapseWidthExample">\
+            <ul class="nav">\
+                <li class="nav-item"><a href="index.html" class="nav-link">首頁</a></li>\
+                <li class="nav-item"><a href="introduce.html" class="nav-link">論壇講座</a></li>\
+                <li class="nav-item"><a href="techNewStars.html" class="nav-link">科技新秀</a></li>\
+                <li class="nav-item"><a href="https://www.technice.com.tw/" class="nav-link" target="_blank">科技新知</a></li>\
+                <li class="nav-item"><a href="javascript:;" class="nav-link" data-bs-toggle="modal" data-bs-target="#exampleModal">駐站專家</a></li>\
+                <li class="nav-item"><a href="javascript:;" class="nav-link" data-bs-toggle="modal" data-bs-target="#exampleModal">菁選企業</a></li>\
+                <li class="nav-item"><a href="aboutTech.html" class="nav-link">關於我們</a></li>\
+                <!-- <li><a href="javascript:;" class="search"><img src="images/icon_search.svg"></a></li> -->\
+            </ul>\
+        </div>\
+    </div>\
+</header>\
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">\
+    <div class="modal-dialog">\
+        <div class="modal-content">\
+            <div class="modal-header">\
+                <a class="btn-close" data-bs-dismiss="modal" aria-label="Close"><img src="images/close.svg"></a>\
+            </div>\
+            <div class="modal-body text-center">\
+                <img src="images/robot_01 1.jpg">\
+                <p>網站建置中，敬請期待...</p>\
+            </div>\
+        </div>\
+    </div>\
+</div>';
+document.write(header);
