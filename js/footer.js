c5e96a94c4e9cef4868d6ab6b7aa29042f0e7d34
weilingshine <weiling.go@gmail.com>
2022-10-03 18:55:21 +0800

    20221003_測試機

--- js/footer.js
+++ js/footer.js
@@ -4,11 +4,11 @@ footer = '<footer>\
             <span class="copyright">copyright © 2022 - 2023 科技專區</span>\
         </div>\
         <ul class="foot-link">\
-            <li><a href="aboutTech.html">關於科技專區</a></li>\
+            <li><a href="/event22/design/technice/aboutTech.html">關於科技專區</a></li>\
             <li><a href="https://www.1111.com.tw/help/privacy.asp" target="_blank">隱私政策</a></li>\
             <li><a href="https://www.1111.com.tw/help/memberRead.asp" target="_blank">會員服務條款</a></li>\
             <!-- <li><a href="#">訂閱電子報</a></li> -->\
-            <li><a href="https://www.facebook.com/technicenews/" class="link-social" target="_blank"><img src="images/icon_facebook.svg">Facebook</a></li>\
+            <li><a href="https://www.facebook.com/technicenews/" class="link-social" target="_blank"><img src="/event22/design/technice/images/icon_facebook.svg">Facebook</a></li>\
         </ul>\
     </div>\
 </footer>';
--- js/header.js
+++ js/header.js
@@ -2,18 +2,18 @@ header = '<header>\
     <div class="container">\
         <div class="mob-header">\
             <a href="javascript:;" class="slidebar-btn" data-bs-toggle="collapse" data-bs-target="#collapseWidthExample" aria-expanded="false" aria-controls="collapseWidthExample"><span></span></a>\
-            <a class="brand-logo" href="https://www.1111.com.tw/" title="1111 科技專區" target="_blank"><img src="images/logo01.png"></a>\
+            <a class="brand-logo" href="https://www.1111.com.tw/" title="1111 科技專區" target="_blank"><img src="/event22/design/technice/images/logo01.png"></a>\
         </div>\
         <div class="sidebar-slide collapse collapse-horizontal" id="collapseWidthExample">\
             <ul class="nav">\
-                <li class="nav-item"><a href="index.html" class="nav-link">首頁</a></li>\
-                <li class="nav-item"><a href="introduce.html" class="nav-link">論壇講座</a></li>\
-                <li class="nav-item"><a href="techNewStars.html" class="nav-link">科技新秀</a></li>\
+                <li class="nav-item"><a href="/event22/design/technice/index.html" class="nav-link">首頁</a></li>\
+                <li class="nav-item"><a href="/event22/design/technice/forum/20221003/introduce.html" class="nav-link">論壇講座</a></li>\
+                <li class="nav-item"><a href="/event22/design/technice/techNewStars.html" class="nav-link">科技新秀</a></li>\
                 <li class="nav-item"><a href="https://www.technice.com.tw/" class="nav-link" target="_blank">科技新知</a></li>\
                 <li class="nav-item"><a href="javascript:;" class="nav-link" data-bs-toggle="modal" data-bs-target="#exampleModal">駐站專家</a></li>\
                 <li class="nav-item"><a href="javascript:;" class="nav-link" data-bs-toggle="modal" data-bs-target="#exampleModal">精選企業</a></li>\
-                <li class="nav-item"><a href="aboutTech.html" class="nav-link">關於我們</a></li>\
-                <!-- <li><a href="javascript:;" class="search"><img src="images/icon_search.svg"></a></li> -->\
+                <li class="nav-item"><a href="/event22/design/technice/aboutTech.html" class="nav-link">關於我們</a></li>\
+                <!-- <li><a href="javascript:;" class="search"><img src="/event22/design/technice/images/icon_search.svg"></a></li> -->\
             </ul>\
         </div>\
     </div>\
@@ -22,10 +22,10 @@ header = '<header>\
     <div class="modal-dialog">\
         <div class="modal-content">\
             <div class="modal-header">\
-                <a class="btn-close" data-bs-dismiss="modal" aria-label="Close"><img src="images/close.svg"></a>\
+                <a class="btn-close" data-bs-dismiss="modal" aria-label="Close"><img src="/event22/design/technice/images/close.svg"></a>\
             </div>\
             <div class="modal-body text-center">\
-                <img src="images/robot_01 1.jpg">\
+                <img src="/event22/design/technice/images/robot_01 1.jpg">\
                 <p>網站建置中，敬請期待...</p>\
             </div>\
         </div>\
